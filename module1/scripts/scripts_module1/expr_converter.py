7c0a5b0d76eeeb850fe6259f0a3d98ac71b20bf0
cmdoret <cyril.mattheydoret@gmail.com>
2016-11-08 10:51:08 +0100

    copied code for indexing genes

--- module1/scripts/scripts_module1/expr_converter.py
+++ module1/scripts/scripts_module1/expr_converter.py
@@ -1,4 +1,6 @@
 import GEOparse
+import pandas as pd
+import numpy as np
 import os
 
 probes_conv = GEOparse.parse_GSM(
@@ -20,3 +22,29 @@ print(char["stage"][3], char["time"][3],char["sex"][3], char["sample_name"][3])
 GPL = gse.gpls["GPL6457"]
 pivoted_samples = gse.pivot_samples('VALUE')
 
+
+strata = pd.read_csv("phylostrata.txt",sep="\t",header=None)
+strata.columns = ["GeneID","ProbeID","age"]
+strata.set_index("ProbeID",inplace=True)
+
+matched_data = pivoted_samples.join(strata, how="inner").groupby(level=0).last()
+
+unique_data = matched_data.groupby("GeneID").mean()
+
+char_pd = pd.DataFrame(char,index=char["sample_name"])
+mixed = char_pd[char_pd.sex == "mixed"].sample_name.tolist()
+mixed += char_pd[char_pd.sex == "female"].sample_name.tolist()
+
+char_pd["timing_number"] = 0
+time_stamps = char_pd.time.unique()
+for i in xrange(len(time_stamps)):
+    char_pd.loc[char_pd.time == time_stamps[i],"timing_number"] = i+1
+
+experiment_index = char_pd[char_pd.index.isin(mixed)].reset_index().groupby("timing_number")["index"].apply(lambda x: np.array(x))
+set_mean = {}
+stages = []
+for d, col_list in experiment_index.iteritems():
+    set_mean[d] = unique_data[col_list].mean(axis=1)
+    stages.append(char_pd[char_pd.index.isin(col_list)].stages[0])
+
+mean_data = pd.DataFrame(set_mean)
